b7c134482e1bb1192ccc9706f74fa764b5ade53b
Heph <srht@mrkeebs.eu>
2022-04-03 21:46:58 +0200

    Added handler interface and filesystem implementations with file-locking

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module git.mrkeebs.eu/debris
 
 go 1.16
-
-require (
-	github.com/gofrs/flock v0.8.1 // indirect
-	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
-)
---
+++ handlers/handlers.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"syscall"
+)
+
+type Handler interface {
+	getStateFile(http.ResponseWriter)
+	updateStateFile(http.Request, http.ResponseWriter)
+	deleteStateFile(http.ResponseWriter)
+}
+
+type Filesystem struct {
+	Filepath string
+}
+
+func (fh Filesystem) GetStateFile(res http.ResponseWriter) {
+	file, err := os.Open(fh.Filepath)
+	if err != nil {
+		log.Println("Error while open file", err)
+	}
+	defer file.Close()
+	res.WriteHeader(200)
+
+	io.Copy(res, file)
+}
+
+func (fh Filesystem) UpdateStateFile(req *http.Request, res http.ResponseWriter) {
+
+	f, err := os.Create(fh.Filepath)
+	if err != nil {
+		log.Println("create file failed", err)
+	}
+	defer f.Close()
+
+	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX); err != nil {
+		log.Println("Add Exclusive Lock failed", err)
+	}
+
+	if _, err := io.Copy(f, req.Body); err != nil {
+		log.Println("Error streaming data", err)
+	}
+
+	res.WriteHeader(200)
+}
+
+func (fh Filesystem) DeleteStateFile(res http.ResponseWriter) {
+	if os.RemoveAll(fh.Filepath) != nil {
+		log.Println("Error while deleting statefile")
+	}
+
+	res.WriteHeader(200)
+}
